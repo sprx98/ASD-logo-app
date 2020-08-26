184de45ac695645d83d9ad1a249b704585893e3b
sprx98 <65811444+sprx98@users.noreply.github.com>
2020-08-26 11:34:39 +0100

    Final version

--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 window.onload = function () {
     document.querySelector('.loader-wrapper').remove();
 }
-
 // CANVAS SETTINGS
 let imageLoader = document.getElementById('img-upload');
     imageLoader.addEventListener('change', handleImage, false);
 let canvas = document.getElementById('img_canvas');
 let ctx = canvas.getContext('2d');
 
-
-
-
-// X = WIDTH && Y = HEIGHT
+// if 0 == black 1 == white
+let logoColor = 0;
+// if 0==
+let logoSide = 0;
 let imgLogo = document.getElementById("logo-top-right");
+imgLogo.crossOrigin = "Anonymous";
+let imgLogo2 = document.getElementById("logo-top-right-white");
+imgLogo2.crossOrigin = "Anonymous";
+
 let logoPosition = [];
 let image = {
     element: '',
@@ -22,15 +25,11 @@ let image = {
 };
 
 //function to load an image and set the src so it gets displayed    
-/* let loadImg = function(event) {
-    image.element = document.getElementById('output');
-    image.element.src = URL.createObjectURL(event.target.files[0]);  
-    init();
-} */
 function handleImage(e){
     let reader = new FileReader();
     reader.onload = function(event){
         image.element = new Image();
+        image.element.crossOrigin = "Anonymous";
         image.element.onload = function(){
             checkImgSize();
             canvas.width = parseInt(image.displaySize[0].replace('px', ''));
@@ -43,12 +42,17 @@ function handleImage(e){
         init(); 
 }
 
+// when img is removed from the canvas redraw it
+function reDraw(){
+    ctx.drawImage(image.element,0,0,parseInt(image.displaySize[0].replace('px', '')),parseInt(image.displaySize[1].replace('px', '')));
+}
+
 // function to check image size
 function checkImgSize() {
 
     image.fullSize = [image.element.naturalWidth, image.element.naturalHeight];
     console.log(image.fullSize + ' this is the full size of the image');
-    if ((image.fullSize[0] < 1200 || image.fullSize[1] < 700) && (image.fullSize[0] != image.fullSize[1])) {
+    if ((image.fullSize[0] < 1000 || image.fullSize[1] < 700) && (image.fullSize[0] != image.fullSize[1])) {
         image.displaySize = [image.fullSize[0] + 'px',image.fullSize[1] + 'px']
         setDisplayStyle(image.displaySize[0],image.displaySize[1])
     }
@@ -60,7 +64,7 @@ function checkImgSize() {
     } 
     // if img is normal
     else if (image.fullSize[0] > image.fullSize[1]){
-        let autoHeight = ((image.fullSize[1] * (1200 / image.fullSize[0])).toFixed(0)).toString() + 'px';
+        let autoHeight = ((image.fullSize[1] * (1000 / image.fullSize[0])).toFixed(0)).toString() + 'px';
         image.displaySize = [('1200px'),autoHeight];
         setDisplayStyle(image.displaySize[0],image.displaySize[1]);
     }
@@ -82,28 +86,70 @@ function clearCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-// displan none on image 
+// displan no image 
 function removeImage(){
     clearCanvas();
 }
-//function to display the logo over the image
-function dispLogo(){
-    //document.getElementById('logo-over').style.display = 'block';
-    logoPosition[0] = (parseInt((image.displaySize[0]).replace('px', ''))-110);
-    logoPosition[1] = (parseInt((image.displaySize[1]).replace('px', ''))-56);
-    ctx.drawImage(imgLogo, logoPosition[0], logoPosition[1],100,46);
-}
 // hide logo
 function hideLogo(){
-    
+    clearCanvas();
+    reDraw();
+}
 
-}
-// ivert logo color
-function logoInvertColor(){
-  
-}
-// logo side switch
+// check the side 
 function logoSideSwitch(){
+    if(logoSide === 0){
+        logoSide = 1;
+        dispLogo(); 
+    } 
+    else if(logoSide === 1){
+        logoSide = 0;
+        dispLogo(); 
+    } 
+} 
+// check the color
+function logoInvertColor(){
+    if(logoColor === 0){
+        logoColor = 1;
+        dispLogo(); 
+    } 
+    else if(logoColor === 1){
+        logoColor = 0;
+        dispLogo(); 
+    } 
+}
+
+function dispLogo(){
+
+    let marginY = 0;
+    let marginX = (parseInt((image.displaySize[1]).replace('px', ''))-56);
+    if(logoSide === 1){
+        marginY = 10;
+    } else{
+        marginY = (parseInt((image.displaySize[0]).replace('px', ''))-110);
+    }
+
+    //when add logo is pressed 
+    clearCanvas();
+    reDraw();
+    ctx.drawImage(imgLogo, marginY, marginX,100,46);
+
+    // when calling a mod function
+    if (logoColor === 0){
+        console.log("Black Logo");
+        clearCanvas();
+        reDraw();
+        ctx.drawImage(imgLogo, marginY, marginX,100,46);
+
+    }
+    else if (logoColor === 1){
+        console.log("White logo");
+        clearCanvas();
+        reDraw();
+        ctx.drawImage(imgLogo2, marginY, marginX,100,46);
+
+
+    }
 
 }
 
@@ -131,10 +177,6 @@ function init(){
         invertLogoColor: 'logo-invert-color',
         exportImage: 'export-img'
     };
-    //remove image hides the img, this line sets it back to block
-    
-    /* check image size when img is loaded */
-    /* image.element.onload = checkImgSize; */
     
     /* if remove image is pressed remov the image */
     document.getElementById(btn.removeImg).addEventListener("click", removeImage);
@@ -143,7 +185,7 @@ function init(){
     document.getElementById(btn.displayLogo).addEventListener("click", dispLogo);
 
     document.getElementById(btn.hideLogo).addEventListener("click", hideLogo);
-    
+
     // change the color of the logo
     document.getElementById(btn.invertLogoColor).addEventListener("click", logoInvertColor);
 
